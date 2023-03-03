87fef7b18ff9152ae602db833075d72b2d24b565
Chidi Arinze <chiddy158@gmail.com>
2023-03-03 17:31:55 +0100

    impliment oop

--- src/duke/choice/Clothing.java
+++ src/duke/choice/Clothing.java
@@ -8,12 +8,22 @@ package duke.choice;
  * @author chiddy
  * @email chiddy158@gmail.com
  */
-public class Clothing {
+public class Clothing implements Comparable<Clothing> {
     private String description;
     private double price;
-    private String size = "M";
-    private final double MIN_PRICE = 10.0;
-    private final double MIN_TAX = 0.2;
+    private String size ;
+    public final static double MIN_PRICE = 10.0;
+    public final static double TAX_RATE = 0.2;
+
+    public Clothing(String description, double price, String size) {
+        this.description = description;
+        this.price = price;
+        this.size = size;
+       
+    }
+    
+    
+    
 
     public String getDescription() {
         return description;
@@ -24,7 +34,7 @@ public class Clothing {
     }
 
     public double getPrice() {
-        return price + (price* MIN_TAX);
+        return price + (price* TAX_RATE);
     }
 
     public void setPrice(double price) {
@@ -37,6 +47,15 @@ public class Clothing {
 
     public void setSize(String size) {
         this.size = size;
+    }
+    
+    @Override
+    public String toString(){
+        return getDescription()+ "." + getSize() + "." + getPrice();    
+    }
+    @Override
+    public int compareTo(Clothing c){
+        return this.description.compareTo(c.description);
     }
     
 }
--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -1,6 +1,8 @@
 
 package duke.choice;
 
+import java.util.Arrays;
+
 /**
  * @author chiddy
  * @email chiddy158@gmail.com
@@ -15,47 +17,25 @@ public class ShopApp {
         double tax = 0.2;
         double total = 0;
         System.out.println("Welcome to Duke choice Shop");
-        Customer c1 = new Customer();
-        c1.setName("Chidi");
-        c1.setSize("S");
+        Customer c1 = new Customer("Chidi",14);
+        System.out.println("Minimum Price: "+ Clothing.MIN_PRICE );
         System.out.println("Welcome "+ c1.getName() + ", Duke Chioce Shop is the shop for all your needs" );
         
-        Clothing item1 = new Clothing();
-        Clothing item2 = new Clothing();
-        Clothing []items = {item1, item2, new Clothing(), new Clothing()};
-        
-        item1.setDescription("Blue Jacket");
-        item1.setPrice(20.9);
-        item1.setSize("M");
-        
-        item2.setDescription("Orange T-Shirt");
-        item2.setPrice(10.5);
-        item2.setSize("S");
-        
-        items[2].setDescription("Green Scarf");
-        items[2].setPrice(5);
-        items[2].setSize("S");
-        
-        items[3].setDescription("Blue T-Shirt");
-        items[3].setPrice(10.5);
-        items[3].setSize("S");
-        
-        //System.out.println("Item 1: "+ item1.description+ ", " + item1.price +", "+item1.size );
-        //System.out.println("Item 2: "+ item2.description+ ", " + item2.price +", "+item2.size );
-        
-        //total = item2.price * 2 + item1.price + (item2.price * 2 + item1.price)*tax;
-        
-        
-        int measurement = 3;
+        Clothing item1 = new Clothing("Blue Jacket",20.9, "M" );
+        Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
+        Clothing []items = {item1, item2, new Clothing("Green Scarf",5, "S" ), new Clothing("Blue T-Shirt",10.5, "S")};
+              
+       // int measurement = 3;
         c1.addItems(items);
-        c1.setSize(measurement);
+       
         System.out.println("Customet is " + c1.getName()+ "," + c1.getSize()+ "," + c1.getTotalClothingCost());
 
         for(Clothing item : c1.getItems()){
-            System.out.println("Items" + item.getDescription());
+            System.out.println("Items output:" + item);
                                           
             }
-            
+           Arrays.sort(c1.getItems());
+           
         }        
         
     }
